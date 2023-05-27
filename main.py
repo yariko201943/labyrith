bbc45227ad982199683aa70990cf38178ea759ba
Yariko <spiyag2014@gmail.com>
2023-05-27 20:44:15 +0300

    add background image

--- main.py
+++ main.py
@@ -5,11 +5,18 @@ pygame.init()
 def file_path(file_name):
     folder = os.path.abspath(__file__ + "/..")
     path = os.path.join(folder, file_name)
+    return path
 
 
 WIN_WIDTH = 900
 WIN_HEIGHT = 600
 FPS = 40
+
+fon = pygame.image.load(file_path(r"images\light_blue.jpg"))
+fon = pygame.transform.scale(fon, (WIN_WIDTH, WIN_HEIGHT))
+
+
+
 
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
 clock = pygame.time.Clock()
@@ -25,7 +32,7 @@ while game:
 
 
     if level == 1:
-        pass
+        window.blit(fon, (0,0))
 
     clock.tick(FPS)
     pygame.display.update()
