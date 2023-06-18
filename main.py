570a273f53eb307e9c4a05c58c6083b3c945b6e9
Yariko <spiyag2014@gmail.com>
2023-06-18 19:53:03 +0300

    new

--- main.py
+++ main.py
@@ -16,7 +16,8 @@ fon = pygame.image.load(file_path(r"images\light_blue.jpg"))
 fon = pygame.transform.scale(fon, (WIN_WIDTH, WIN_HEIGHT))
 
 class Game_sprite(pygame.sprite.Sprite):
-    def init(self, x, y, width, height, image):
+    def __init__(self, x, y, width, height, image):
+        super().__init__()
         self.rect = pygame.Rect(x, y, width, height)
         self.image = pygame.image.load(file_path(image))
         self.image = pygame.transform.scale(self.image, (width, height))
@@ -25,9 +26,27 @@ class Game_sprite(pygame.sprite.Sprite):
         window.blit(self.image, (self.rect.x, self.rect.y))
 
 
-player = Game_sprite(40, 420, 55, 50,(file_path(r"images\pac-man-icon-13.jpg")))
-enemy = Game_sprite(324, 60, 27, 50,(file_path(r"images\Pink_ghost.png")))
-finish = Game_sprite(400, 10, 47, 30,(file_path(r"images\Cherry.png")))
+
+class Player(Game_sprite):
+    def __init__(self, x, y, width, height, image, speedx, speedy):
+        super().__init__(x, y, width, height, image)
+        self.speedx = speedx
+        self.speedy = speedy
+        self.direction = "Right"
+        self.image_r = self.image
+        self.image_l = pygame.transform.flip(self.image, True, False)
+
+    def update(self):
+        if self.speedx < 0 and self.rect.left > 0 or self.speedx > 0 and self.rect.right < WIN_WIDTH:
+            self.rect.x += self.speedx
+        if self.speedy < 0 and self.rect.top > 0 or self.speedy > 0 and self.rect.bottom < WIN_HEIGHT:
+            self.rect.y += self.speedy
+        
+
+
+player = Player(40, 420, 55, 50, r"images\pac-man-icon-13.jpg", 5, 0)
+enemy = Game_sprite(400, 300, 50, 50, r"images\Robo_ghost.png")
+finish = Game_sprite(700, 100, 50, 30, r"images\Cherry.jpg")
 
 
 window = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
@@ -41,11 +60,37 @@ while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
-
+        if level == 1:    
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_d:
+                    player.speedx = 4
+                    player.direction = "Right"
+                    player.image = player.image_r
+                if event.key == pygame.K_a:
+                    player.speedx = -4
+                    player.direction = "Left"
+                    player.image = player.image_l
+                if event.key == pygame.K_w:
+                    player.speedy = -4
+                if event.key == pygame.K_s:
+                    player.speedy = 4
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_d:
+                    player.speedx = 0
+                if event.key == pygame.K_a:
+                    player.speedx = 0
+                if event.key == pygame.K_w:
+                    player.speedy = 0
+                if event.key == pygame.K_s:
+                    player.speedy = 0 
 
     if level == 1:
         window.blit(fon, (0,0))
+        player.show()
+        player.update()
+        enemy.show()
+        finish.show()
+        
 
     clock.tick(FPS)
     pygame.display.update()
-
